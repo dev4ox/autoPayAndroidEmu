005bcc653f226c1f0a1e97963a2e57905122b54f
dev4ox <dev4ox@gmail.com>
2025-04-24 22:53:07 +0300

    edit settings

--- settings.py
+++ settings.py
@@ -3,17 +3,20 @@ MEMU_ADB_PORTS = {
     "MEmu1": "127.0.0.1:21503",
     "MEmu2": "127.0.0.1:21513",
     "MEmu3": "127.0.0.1:21523",
+
     # добавь свои при необходимости
 }
+
+# Вставляем токен бота из @Botfather
 BOT_TOKEN = ''
 
-ADB_PATH = ''
+ADB_PATH = 'C:/Program Files/Microvirt/MEmu/adb.exe'
 
-MEMU_VM_NAME = ''
+MEMU_VM_NAME = 'MEmu1'
 
-SAVE_FOLDER = ''
+SAVE_FOLDER = 'qr_codes'
 
+# Должно быть в формате: -100**********
 CHAT_ID = -100
 
 BANK = 'tinkoff'
-
